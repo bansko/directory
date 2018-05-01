3919bde5f42942ec202a632d8949433bb68d457c
Rob Thijssen <rthijssen@gmail.com>
2018-05-01 13:01:21 +0300

    added a tag cloud

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,47 @@
  * Controller of the directoryApp
  */
 angular.module('directoryApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    function getRandomSize(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    function shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+      }
+      return a;
+    }
+    $scope.tags = shuffle([
+      'health',
+      'walking',
+      'hiking',
+      'trekking',
+      'mindfulness',
+      'meditation',
+      'relaxation',
+      'cycling',
+      'rafting',
+      'boating',
+      'running',
+      'jogging',
+      'fitness',
+      'yoga',
+      'pilates',
+      'spa',
+      'gym',
+      'swimming',
+      'exercise',
+      'mindfulness',
+      'mountain biking',
+      'enduro',
+      'trail',
+      'sauna',
+      'jacuzzi',
+      'steam bath',
+      'hot spring'
+    ]).map(function(x){ return {text: x, size: getRandomSize(6)};});
   });
