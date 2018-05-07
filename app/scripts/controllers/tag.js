00bf23ca4aab6bc6e8c0751d8be713fe4ec2dcdb
Rob Thijssen <rthijssen@gmail.com>
2018-05-07 19:46:38 +0300

    tags

--- app/scripts/controllers/tag.js
+++ app/scripts/controllers/tag.js
@@ -11,7 +11,7 @@ angular.module('directoryApp')
   .controller('TagCtrl', function ($scope, $routeParams, BusinessApi) {
     $scope.tagId = $routeParams.tagId;
     BusinessApi.query({}, function (businesses) {
-      $scope.businesses = businesses.filter(function (x) { return x.tags.includes($routeParams.tagId); });
+      $scope.businesses = businesses.filter(function (x) { return x.live && x.tags.includes($routeParams.tagId); });
       $scope.businesses = $scope.businesses.map(function(x, i) {
         BusinessApi.get({businessId: x.id}, function(b) {
           $scope.businesses[i] = Object.assign($scope.businesses[i], b)
