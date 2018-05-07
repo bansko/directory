05e5f57b20f9537d31a0ab0fc710dbe5c0e24dbc
Rob Thijssen <rthijssen@gmail.com>
2018-05-07 16:43:34 +0300

    tag cloud font size calculation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,7 +23,22 @@ angular.module('directoryApp')
       return a;
     }
     BusinessApi.query({}, function (businesses) {
-
-      $scope.tags = shuffle(Array.from(new Set([].concat.apply([], businesses.map(function(x){ return x.tags;}))))).map(function(x){ return {text: x, size: getRandomSize(6)};});
+      var min = 0, max = 0;
+      var counts = {};
+      var allTags = [].concat.apply([], businesses.map(function(x){ return x.tags;}));
+      allTags.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+      for (var key in counts) {
+        if (Object.prototype.hasOwnProperty.call(counts, key)) {
+          max = Math.max(max, counts[key]);
+          min = Math.min(min, counts[key]);
+        }
+      }
+      $scope.tags = shuffle(Array.from(new Set(allTags))).map(function(x) {
+        return {
+          text: x,
+          count: counts[x],
+          size: Math.round((counts[x] / max) * 5)
+        };
+      });
     });
   });
